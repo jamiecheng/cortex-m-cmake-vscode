9018e9a451fe13791b62b0c3575d893d7fbfedc4
jamiecheng <jamiecheng97@gmail.com>
2022-06-02 22:08:38 +0200

    added main example

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,26 @@
 #include "system.h"
 #include "delay.h"
+#include "gpio.h"
 
-int main() {
+#define LED_PORT 0
+#define LED_PIN 1
+
+static void config_peripherals()
+{
     system_init();
+
+    gpio_config_input(LED_PORT, LED_PIN, GPIO_MODE_NO_PULL);
+}
+
+int main()
+{
+    config_peripherals();
 
     while (1)
     {
-        delay(1000);
+        gpio_write(LED_PORT, LED_PIN, 1);
+        delay(500);
+        gpio_write(LED_PORT, LED_PIN, 0);
+        delay(500);
     }
 }
